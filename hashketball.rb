55c1ef42c4e17d9b3ba92caf00ab5b586d9c037d
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-09 15:09:10 -0400

    adding updated first 3 deliverables/7

--- hashketball.rb
+++ hashketball.rb
@@ -147,4 +147,12 @@ def shoe_size(player_name)
       end
     end
   end
+
+def team_colors(team_name)
+  game_hash.each do |_team, team_info|
+    if (team_info[:team_name] == team_name)
+      return team_info[:colors]
+    end
+  end
+end
 # Write code here
